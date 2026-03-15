2ecc4a0b88c9d968c12ce09a87195fed4f1af3f5
Ben Summers <bensummersonline@gmail.com>
2026-03-15 19:37:09 +0000

    colours and deploying python

--- api/main.py
+++ api/main.py
@@ -7,7 +7,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://torgide.com",
+        "https://www.torgide.com",
+        "http://localhost:5173",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
